391b036ef168fa7484a0cc132e78f220daaf16fe
Yusuke_Saito <ysaito8015@gmail.com>
2023-06-09 23:48:26 +0900

    amend

--- scripts/01_individual.R
+++ scripts/01_individual.R
@@ -1,6 +1,9 @@
-if (!require("pacman")) {
-  install.packages("pacman")
-}
+rm(list = ls())
+gc()
+
+source("./environments.R")
+.libPaths(libDir)
+
 pacman::p_load(
   tidyverse,
   rstan,
@@ -8,12 +11,20 @@ pacman::p_load(
   rio
 )
 
-rstan_options(auto_write = TRUE)
+rstan::rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-data.pk   <- rio::import("../data/sim_pk_20170521.csv") 
+figDir <- file.path(outDir, "01")
+if (!dir.exists(figDir)) {
+  dir.create(figDir)
+}
+
+
+data.pk <-
+  rio::import(file.path(dataDir, "sim_pk_20170521.csv") )
 # dbl (4): ID, TIME, CONC, DOSE_LEVEL
-data.subj <-rio::import("../data/subj_dose_20170521.csv") 
+data.subj <-
+  rio::import(file.path(dataDir, "subj_dose_20170521.csv")) 
 # dbl (2): ID, DOSE
 
 cat("\n\n")
@@ -25,22 +36,29 @@ print("summary of subject with dose data")
 print(summary(data.subj))
 print(head(data.subj))
 
-pdf("../outputs/01/01-data-pk-id1.pdf")
+p <-
   data.pk %>%
-    filter(ID == 1) %>% 
-    ggplot(aes(TIME, CONC)) + # CONC: concentration in blood
-    geom_line() +
-    geom_point() 
-dev.off()
+  filter(ID == 1) %>% 
+  ggplot(aes(TIME, CONC)) + # CONC: concentration in blood
+  geom_line() +
+  geom_point() 
+ggsave(
+  file.path(figDir, "01-data-pk-id1.pdf"),
+  plot = p
+)
 
-pdf("../outputs/01/01-data-pk-facet-by-dose.pdf")
+p <-
   full_join(data.pk, data.subj) %>% 
-    ggplot(aes(TIME, CONC, group=ID)) +
-    geom_line() +
-    geom_point() +
-    facet_wrap(~DOSE) +
-    scale_y_log10()
-dev.off()
+  ggplot(aes(TIME, CONC, group=ID)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~DOSE) +
+  scale_y_log10()
+ggsave(
+  file.path(figDir, "02-data-pk-facet-by-dose.pdf"),
+  plot = p
+)
+
 
 data.pk.id1 <- data.pk %>% filter(ID == 1)
 
@@ -50,31 +68,61 @@ print(head(data.pk.id1))
 
 # Initial states of variables
 init <- function(){
-    list(KA = exp(rnorm(1, log(0.5), 0.2)), # Absorption rate constant (k_a)
-         CL = exp(rnorm(1, log(0.5), 0.2)), # Clearance (CL)
-         VD = exp(rnorm(1, log(5), 0.2)),   # Volume of distribution (Vd)
-         s_Y = runif(1, 0.5, 2))            # standard deviation of Y in log scale (observation, measured conc.)
+  list(
+    KA =
+      exp(
+        rnorm(
+          n = 1,
+          mean = log(0.5),
+          sd = 0.2
+        )
+      ), # Absorption rate constant (k_a)
+    CL =
+      exp(
+        rnorm(
+          n = 1,
+          mean = log(0.5),
+          sd = 0.2
+        )
+      ), # Clearance (CL)
+    VD =
+      exp( 
+        rnorm(
+          n = 1,
+          mean = log(0.5),
+          sd = 0.2
+        )
+      ), # Volume of distribution (Vd)
+    sd_Y =
+      runif(
+         n = 1,
+         min = 0.5,
+         max = 2
+       ) # standard deviation of Y in log scale (observation, measured conc.)
+  )
 }
 
 # Join data into list class for stan execution
 data <- 
-  list(N    = nrow(data.pk.id1),
-       TIME = data.pk.id1$TIME,
-       DOSE = 10,
-       Y    = data.pk.id1$CONC)
+  list(
+    N    = nrow(data.pk.id1),
+    TIME = data.pk.id1$TIME,
+    DOSE = 10,
+    Y    = data.pk.id1$CONC
+  )
 
 cat("\n\n")
 print("list data for stan")
-print(head(data))
+print(str(data))
 
 
 # Execute stan
 fit.stan <-
   stan(
-    file = "../stan_codes/mod_00_single_subj.stan", 
+    file = file.path(stanDir, "mod_00_single_subj.stan"),
     data = data,
     init = init
-  )
+)
 
 
 cat("\n\n")
@@ -90,39 +138,48 @@ list.param <- c("KA", "CL", "VD", "s_Y")
 fit.param.plot <- 
   fit.param %>% filter(Parameter %in% c(list.param))
 
-pdf("../outputs/01/01-posterior-density-plot.pdf")
-  ggs_density(fit.param.plot)
-dev.off()
+p <- ggs_density(fit.param.plot)
+ggsave(
+  file.path(figDir, "03-posterior-density-plot.pdf"),
+  plot = p
+)
 
-pdf("../outputs/01/01-posterior-trace-plot.pdf")
-  ggs_traceplot(fit.param.plot)
-dev.off()
+p <- ggs_traceplot(fit.param.plot)
+ggsave(
+  file.path(figDir, "04-posterior-trace-plot.pdf"),
+  plot = p
+)
 
-pdf("../outputs/01/01-posterior-autocorrelation-plot.pdf")
-  ggs_autocorrelation(fit.param.plot)
-dev.off()
+p <- ggs_autocorrelation(fit.param.plot)
+ggsave(
+  file.path(figDir, "05-posterior-autocorrelation-plot.pdf"),
+  plot = p
+)
 
-pdf("../outputs/01/01-posterior-rhat-plot.pdf")
-  ggs_Rhat(fit.param.plot)
-dev.off()
-
+p <- ggs_Rhat(fit.param.plot)
+ggsave(
+  file.path(figDir, "06-posterior-rhat-plot.pdf"),
+  plot = p
+)
 
 
 mcmc.sample <- rstan::extract(fit.stan)
 
 y.pred.interval <- 
   mcmc.sample$y_new %>% 
-  apply(MARGIN=2, quantile, prob=c(0.05, 0.5, 0.95)) %>% 
+  apply(MARGIN = 2, quantile, prob = c(0.05, 0.5, 0.95)) %>% 
   t() %>% 
   tibble::as_tibble()
 
-print(y.pred.interval)
+print(head(y.pred.interval))
 
-pdf("../outputs/01/01-estimate-mean-values-with-predicted-interval.pdf")
-  bind_cols(data.pk.id1,
-            y.pred.interval) %>% 
-    ggplot(aes(TIME, `50%`)) +
-    geom_line() +
-    geom_ribbon(aes(ymin=`5%`, ymax=`95%`),alpha=0.1) +
-    geom_point(data=data.pk.id1, aes(TIME,CONC))
-dev.off()
+p <-
+  bind_cols(data.pk.id1, y.pred.interval) %>% 
+  ggplot(aes(TIME, `50%`)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = `5%`, ymax = `95%`),alpha = 0.1) +
+  geom_point(data=data.pk.id1, aes(TIME,CONC))
+ggsave(
+  file.path(figDir, "07-estimate-mean-values-with-predicted-interval.pdf"),
+  plot = p
+)
